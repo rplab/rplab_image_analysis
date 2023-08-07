04c4461d2f319ff94baa0650eee31a48cb41829e
Jonah Sokoloff <jonahs@uoregon.edu>
2023-08-07 15:17:01 -0700

    updated due to file module changes.

--- rplab_image_analysis/utils/metadata.py
+++ rplab_image_analysis/utils/metadata.py
@@ -2,51 +2,51 @@ import json
 import pathlib
 from ast import literal_eval
 from configparser import ConfigParser
-from utils.utils import is_mm_metadata, is_ls_pycro_metadata
+from utils.files import FileSubtype, get_file_subtype
 
 class MMMetadata(object):
     def __init__(self, directory: str):
-        self._init_metadata_dict(directory)
+        self._metadata_dict: dict = self._get_metadata_dict(directory)
         self.summary_metadata: dict = self._metadata_dict["Summary"]
-        self.image_key_frames: list = [key for key in self._metadata_dict if "FrameKey" in key]
+        self.key_frames: list = [key for key in self._metadata_dict if "FrameKey" in key]
 
-    def _init_metadata_dict(self, directory):
-        if is_mm_metadata(directory):
-            self._metadata_dict: dict = json.load(open(directory))
+    def _get_metadata_dict(self, directory: str) -> dict:
+        if get_file_subtype(directory) == FileSubtype.METADATA:
+            return json.load(open(directory))
         else:
-            file_list = [str(file) for file in pathlib.Path(directory).iterdir()]
-            for file in file_list:
-                if is_mm_metadata(file):
-                    self._metadata_dict: dict = json.load(open(file))
-                    break
-    
-    def get_image_metadata(self, image_num):
-        return self._metadata_dict[self.image_key_frames[image_num]]
+            parent_file_generator = pathlib.Path(directory).parent.iterdir()
+            parent_files = [str(file) for file in parent_file_generator]
+            for file in parent_files:
+                if get_file_subtype(file) == FileSubtype.METADATA:
+                    return json.load(open(file))
+  
+    def get_image_metadata(self, image_num: int) -> dict:
+        return self._metadata_dict[self.key_frames[image_num]]
 
 
 class LSPycroMetadata(object):
     def __init__(self, directory: str):
-        super().__init__()
-        self._init_config(directory)
         self._config = ConfigParser()
+        self._init_config(directory)
 
-    def _init_config(self, directory):
-        if is_ls_pycro_metadata(directory):
+    def _init_config(self, directory: str):
+        if get_file_subtype(directory) == FileSubtype.LS_NOTES:
             self._config.read(directory)
         else:
-            file_list = [str(file) for file in pathlib.Path(directory).iterdir()]
-            for file in file_list:
-                if is_ls_pycro_metadata(file):
+            file_generator = pathlib.Path(directory).iterdir()
+            files = [str(file) for file in file_generator]
+            for file in files:
+                if get_file_subtype(file) == FileSubtype.LS_NOTES:
                     self._config.read(file)
                     break
     
-    def get_section_dict(self, section):
+    def get_section_dict(self, section: str) -> dict:
         section_dict = {}
         for item in self._config.items(section):
             section_dict[item[0]] = literal_eval(item[1])
         return section_dict
     
-    def get_region_dict(self, fish_num, region_num):
+    def get_region_dict(self, fish_num: int, region_num: int) -> dict:
         section = f"Fish {fish_num} Region {region_num}"
         return self.get_section_dict(section)
 
