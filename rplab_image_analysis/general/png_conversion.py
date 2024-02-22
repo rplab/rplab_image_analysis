38ef48c536fe81a257efb34a917a702b03fd7004
Jonah Sokoloff <jonahs@uoregon.edu>
2024-02-21 16:28:45 -0800

    simplified process and now iterates through generator.

--- rplab_image_analysis/general/png_conversion.py
+++ rplab_image_analysis/general/png_conversion.py
@@ -1,130 +1,142 @@
-import os
-import numpy as np
 import pathlib
-import psutil
-import skimage.io
-import utils.files as files
 from functools import partial
 from multiprocessing import Pool
+
+import psutil
 from tifffile import TiffFile
-from utils.metadata import MMMetadata
+
+from rplab_image_analysis.utils import files, metadata, file_types
 
 
-PNG = files.ImageFileType.PNG.value[0]
+PNG = file_types.ImageFileType.PNG.value[0]
+PNGS = "_pngs"
 
 
-def batch_convert_to_pngs(source_dir: str | pathlib.Path, 
-                          dest_dir: str | pathlib.Path
-                          ) -> str:
+def png_convert_batch(source_dir: str | pathlib.Path, 
+                      dest_dir: str | pathlib.Path
+                      ) -> str:
+    """
+    Converts all images in source_dir and all subdirectories to PNGs
+    in dest_dir. Also copies over directory structure of source_dir and copies 
+    over non-image files.
+
+    ### Parameters:
+
+    source_dir: str
+        source directory of images.
+
+    dest_dir: str
+        directory where all images will be saved to.
+
+    ### Returns:
+    
+    dest_path: str
+        returns directory where images were written to.
+    """
     source_path = pathlib.Path(source_dir)
-    dest_path = files.get_batch_dest_path(source_path, dest_dir, 
-                                          suffix = "_pngs")
-    dirs = []
-    for root, directories, filenames in os.walk(source_dir):
-        for filename in filenames:
-            dirs.append(root)
-    dirs = set(dirs)
-    if len(dirs) != 0:
-        files.shutil_copy_ignore_images(source_path, dest_path)
-        _start_multiprocess(source_path, dest_path, dirs)
-    return str(dest_path)      
+    dest_path = files.get_batch_path(source_path, dest_dir, PNGS)
+    files.copytree_ignore_images(source_path, dest_path)
+    dirs = files.yield_roots(source_path)
+    _start_process(source_path, dest_path, dirs)
+    return str(dest_path)
 
 
-def _start_multiprocess(source_path: pathlib.Path, 
+def convert_tif_to_png(file_path: str | pathlib.Path,
+                       save_dir: str | pathlib.Path
+                       ) -> str:
+    """
+    Converts tif image or stack to PNG(s).
+
+    ### Parameters:
+
+    file_path: str
+        file path of image file.
+
+    save_dir: str
+        directory for images to be saved to. If directory does not exist,
+        will be created.
+    """
+    file_path = pathlib.Path(file_path)
+    save_dir = pathlib.Path(save_dir)
+    save_dir.mkdir(exist_ok=True)
+    files.copy_non_image_files(file_path.parent, save_dir)
+    save_path = save_dir.joinpath(file_path.name)
+    _convert_tif(file_path, save_path)
+
+
+def _start_process(source_path: pathlib.Path, 
                         dest_path: pathlib.Path, 
                         dirs: list[pathlib.Path]):
     """
     Starts multiprocess to utilize full CPU when performing PNG conversion.
     """
-    with Pool(psutil.cpu_count(logical=False)) as pool:
-        pool_func = partial(
-            _png_conversion_task, source_path, dest_path)
-        pool.map(pool_func, dirs)
+    num_cores = psutil.cpu_count(logical=False)
+    with Pool(num_cores) as pool:
+        pool_func = partial(_png_conversion_task, source_path, dest_path)
+        #imap used to do lazy loading of dirs so we aren't taking up a ton of
+        #memory with a list of strings. Also, calling just imap() doesn't seem
+        #to start processing pool. Iterating works fine, though there may be
+        #side effects of this.
+        for result in pool.imap(pool_func, dirs):
+            pass
 
 
 def _png_conversion_task(source_path: pathlib.Path, 
                          dest_path: pathlib.Path, 
-                         dir: pathlib.Path):
-    file_paths = files.get_image_files_in_dir(dir)
-    if files.get_file_type(file_paths[0]) == files.ImageFileType.TIF:
-        _tif_png_conversion_task(file_paths, dest_path, source_path)
-
-
-def _tif_png_conversion_task(file_paths: list[pathlib.Path], 
-                             dest_path: pathlib.Path, 
-                             source_path: pathlib.Path):
-    #keep track of page_num outside of file loop in case there are multiple 
-    #tif files in a single directory.
-    first_path = file_paths[0]
-    page_num = 0
-    for file_path in file_paths:
-        stack = TiffFile(file_path)
-        is_single_image = _get_stack_num_dims(stack) == 2
-        if is_single_image:
-            save_path = _get_single_save_path(
-                file_path, source_path, dest_path)
-            image = stack.asarray()
-            _write_png(save_path, image)
+                         dir: pathlib.Path
+                         ):
+    """
+    Iterative task used by "worker threads" to convert images to PNGs.
+    """
+    for image_file in files.get_image_files(dir):
+        save_path = files.get_save_path(image_file, source_path, dest_path)
+        if file_types.is_tif(image_file):
+            _convert_tif(image_file, save_path)
         else:
-            for page in stack.pages:
-                save_path = _get_stack_save_path(
-                    first_path, source_path, dest_path, page_num)
-                image = page.asarray()
-                _write_png(save_path, image)
-                page_num += 1
+            image = files.read_image(image_file)
+            files.save_image(save_path.with_suffix(PNG), image)
 
 
-def _get_stack_num_dims(stack: TiffFile):
-    return len(stack.series[0].shape)
+def _convert_tif(file_path: pathlib.Path, save_path: pathlib.Path):
+    """
+    Converts tif to PNG(s).
+    """
+    if metadata.is_mm(file_path):
+        _convert_mm_tif(file_path, save_path)
+    else:
+        _convert_misc_tif(file_path, save_path)
 
 
-def _get_single_save_path(file_path: pathlib.Path, 
-                          source_path: pathlib.Path, 
-                          dest_path: pathlib.Path
-                          ) -> pathlib.Path:
-    new_name = _get_single_image_name(file_path)
-    rel_path = _get_rel_path(source_path, file_path, new_name)
-    return dest_path.joinpath(rel_path)
+def _convert_mm_tif(file_path: pathlib.Path, save_path: pathlib.Path):
+    """
+    converts tif with MM metadata.
+    """
+    try:
+        meta = metadata.MMMetadata(file_path)
+    except FileNotFoundError:
+        _convert_misc_tif(file_path)
+    else:
+        pages = TiffFile(file_path).pages
+        #first filename in metadata is what will be used for all files that 
+        #share the metadata
+        filename = meta.all_filenames[0]
+        page_num = meta.get_filename_start_num(file_path.name)
+        for page in pages:
+            save_path = save_path.with_name(filename)
+            if coords := meta.get_image_metadata(page_num).get_coords_str():
+                save_path = save_path.with_stem(f"{save_path.stem}_{coords}")
+            save_path = save_path.with_suffix(PNG)
+            files.save_image(save_path, page.asarray())
+            page_num += 1
 
 
-def _get_single_image_name(file_path: pathlib.Path) -> str:
-    old_name = file_path.name
-    new_name = files.remove_mmstack(old_name)
-    new_name = files.remove_image_extn(old_name)
-    return new_name
-
-
-def _get_rel_path(source_path: pathlib.Path, 
-                  file_path: pathlib.Path, 
-                  new_name: str
-                  ) -> pathlib.Path:
-    old_name = file_path.name
-    rel_path = file_path.relative_to(source_path)
-    return pathlib.Path(str(rel_path).replace(old_name, new_name))
-
-
-def _get_stack_save_path(file_path: pathlib.Path, 
-                         source_path: pathlib.Path, 
-                         dest_path: pathlib.Path, 
-                         page_num: int
-                         ) -> pathlib.Path:
-    new_name = _get_stack_image_name(file_path, page_num)
-    rel_path = _get_rel_path(source_path, file_path, new_name)
-    return dest_path.joinpath(rel_path)
-
-
-def _get_stack_image_name(file_path: pathlib.Path, page_num: int) -> str:
-    name = file_path.name
-    name = files.remove_mmstack(name)
-    name = files.remove_image_extn(name)
-    metadata = MMMetadata(file_path).get_image_metadata(page_num)
-    coords = metadata.get_coords_str()
-    return f"{name}_{coords}{PNG}"
-
-
-def _write_png(save_path: pathlib.Path, image: np.ndarray):
-    if not save_path.exists():
-        skimage.io.imsave(
-            fname=save_path, arr=image, check_contrast=False, plugin='imageio', 
-            compress_level=3)
-    
+def _convert_misc_tif(file_path: pathlib.Path, save_path: pathlib.Path):
+    """
+    converts tif without MM metadata.
+    """
+    pages = TiffFile(file_path).pages
+    for page_num, page in enumerate(pages):
+        if coords := f"{page_num + 1}" if page_num else "":
+            save_path = save_path.with_stem(f"{save_path.stem}_{coords}")
+        save_path = save_path.with_suffix(PNG)
+        files.save_image(save_path, page.asarray())
